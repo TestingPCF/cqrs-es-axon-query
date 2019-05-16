0387731a4df2c1b55872d9780436274e2aef2e22
Anil Kumar <k_anil@HCL.COM>
2019-05-16 15:15:03 +0530

    first commit

---
+++ src/main/java/com/hcl/cqrs/QuerySideApplication.java
@@ -0,0 +1,20 @@
+package com.hcl.cqrs;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableJpaRepositories
+public class QuerySideApplication {
+	private static final Logger LOG = LoggerFactory.getLogger(QuerySideApplication.class);
+	public static void main(String[] args) {
+		SpringApplication.run(QuerySideApplication.class, args);
+		LOG.info("Starting the QUERY-SIDE PCF Axon CQRS Demo [The Catalog Viewer] with SpringBoot.");
+	}
+
+}
